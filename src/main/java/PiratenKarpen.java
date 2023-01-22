72e89334a864b81f6f7311736e2718f8b1511a0c
HagOrMan <khagypi3@gmail.com>
2023-01-22 13:21:46 -0500

    Implement method to check number of skulls rolled and prints it

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,6 +1,7 @@
 import pk.Dice;
 import pk.Faces;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PiratenKarpen {
 
@@ -13,7 +14,8 @@ public class PiratenKarpen {
         System.out.println("\nHaha, tricked you!! We're back, rolling 8 dice this time.");
     }
 
-    public static ArrayList<Faces> rollDice(Dice myDice, ArrayList<Faces> rolls){
+    // Rolls dice based on how many rolls are left until all 8 dice have been rolled.
+    public static void rollDice(Dice myDice, ArrayList<Faces> rolls){
         for (int i = rolls.size(); i < 8; i++){
             rolls.add(myDice.roll());
             if (i != 7)
@@ -21,15 +23,35 @@ public class PiratenKarpen {
             else
                 System.out.print(rolls.get(i));
         }
-        return rolls;
     }
 
+    // Allows user to take one turn until they stop rolling dice.
     public static void takeTurn(Dice myDice, String player){
 
-        System.out.printf("Player $s is now rolling... \n", player);
+        // Does the first roll for the user.
+        System.out.printf("Player %s is now rolling... \n", player);
         ArrayList<Faces> rolls = new ArrayList<>();
         rollDice(myDice, rolls);
 
+        // Checks how many skulls there are.
+        int numSkulls = checkSkulls(rolls);
+        if (numSkulls > 2){
+            System.out.printf("Sorry, your turn is over!! You rolled %d skulls... \n", numSkulls);
+        }
+
+        // Lets the user keep rolling until satisfied or 3 skulls have been gotten.
+
+
+    }
+
+    // Returns the number of skulls in the list.
+    public static int checkSkulls(ArrayList<Faces> rolls){
+        int counter = 0;
+        for (int i = 0; i < rolls.size(); i++){
+            if (rolls.get(i) == Faces.SKULL)
+                counter++;
+        }
+        return counter;
     }
     
 }
