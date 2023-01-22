ddb868c40ae7766ce75f6cc611125691c46a1c06
HagOrMan <khagypi3@gmail.com>
2023-01-22 12:50:05 -0500

    Allows user to roll 8 dice and see output

--- logbook.md
+++ logbook.md
@@ -5,4 +5,4 @@ Format: Date | Description of work
 2023-01-13 | Created logbook, added to git repository, made skeleton of the report
 2023-01-17 | Answered questions in report for step 1, with minimal words
 2023-01-20 | Went back to answers in report, changed some, and gave much more detail. Also worked on step 2, editing backlog.
-2023-01-22 | Finished F01 on the 21st, but only got around to committing and pushing to github today.
+2023-01-22 | Finished F01 on the 21st, but only got around to committing and pushing to github today. Delivered F02.
--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -8,8 +8,17 @@ public class PiratenKarpen {
         Dice myDice = new Dice();
         System.out.println(myDice.roll());
         System.out.println("That's all folks!");
+        System.out.println("\nHaha, tricked you!! We're back, rolling 8 dice this time.");
+        roll8Dice(myDice);
     }
 
-    
+    public static void roll8Dice(Dice myDice){
+        for (int i = 0; i < 8; i++){
+            if (i != 7)
+                System.out.print(myDice.roll() + ", ");
+            else
+                System.out.print(myDice.roll());
+        }
+    }
     
 }
